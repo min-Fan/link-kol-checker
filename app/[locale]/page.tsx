7c80e954b0122cef0b6d9728ecca18e90acfcc1b
min-Fan <1258162171@qq.com>
2025-08-22 17:09:15 +0800

    feat✨: mini app config cli

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,5 +1,14 @@
+"use client";
 import Home from "./main/page";
+import { useEffect } from "react";
+import { useMiniKit } from "@coinbase/onchainkit/minikit";
 
 export default function HomePage() {
+  const { setFrameReady, isFrameReady } = useMiniKit();
+
+  useEffect(() => {
+    if (!isFrameReady) setFrameReady();
+  }, [isFrameReady, setFrameReady]);
+
   return <Home />;
 }
