43e1dda1be3cb1f23be2d2c45747c77ff713dae4
min-Fan <1258162171@qq.com>
2025-08-22 17:18:35 +0800

    fix🐛: generateMetadata upd version

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -54,7 +54,7 @@ export async function generateMetadata(): Promise<Metadata> {
     description: 'LinkKol Checker',
     other: {
       'fc:frame': JSON.stringify({
-        version: 'next',
+        version: '1.0.0',
         imageUrl: process.env.NEXT_PUBLIC_APP_HERO_IMAGE,
         button: {
           title: `Launch ${process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME}`,
